e1f0e9e7affcf88583024800a810632056611501
Fernando Galvan <n.brighter00@gmail.com>
2022-09-20 12:22:12 -0500

    finished displaying information

--- home/views.py
+++ home/views.py
@@ -10,10 +10,22 @@ API_KEY = env('API_KEY')
 # Create your views here.
 def search(request):
     query = request.GET.get ('q')
+    results = []
     if query:
         data = requests.get(f"https://api.themoviedb.org/3/search/tv?api_key={API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
-    else: 
-        return HttpResponse("Please enter a search query")
+        
+
+    #     temp = []
+    #     for d in data["results"]:
+    #         if len(temp) < 3:
+    #             temp.append({"name": d["name"], "poster_path": d["poster_path"], "overview": d["overview"]})
+    #         else:
+    #             results.append(temp)
+
+    #     results.append(temp) if len(temp) > 0 else None
+
+    # else: 
+    #     return HttpResponse("Please enter a search query")
         
     return render(request, 'home/results.html', {
         "data": data.json()
