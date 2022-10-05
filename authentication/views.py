5b3d3db7cf0466693d8ca9b7b72608fe97518882
Fernando Galvan <n.brighter00@gmail.com>
2022-10-05 11:49:37 -0500

    added requirements

--- authentication/views.py
+++ authentication/views.py
@@ -1,17 +1,29 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpRequest
+from django.http import HttpResponse
 # Create your views here.
 def register(request):
     if request.method == "Post":
         user = User.objects.create_user(email=request.POST["email"], password=request.POST["passwrod"])
         
         login(request, user)
-        return HTTPResponse("success")
+        return HttpResponse("success")
     
     else:
         return render(request, "authentication/register.html")
 
-def login_view()
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = authenticate(email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+        else:
+            return redirect("/")
+    else:
+        return render(request, "authentication/login.html")
